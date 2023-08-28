5a64abbc6bb7085895b856e2645c60116f05b4e5
mportilla <mothaccounts@gmail.com>
2023-08-27 21:00:49 -0400

    se trabaja en archivos.jsx y da estilo css

---
+++ desafioCyE/src/components/Registro.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import SocialButton from './SocialButton'
+import Formulario from './Formulario'
+import Alert from './Alert'
+
+const Registro = () => {
+    const iconos = ['facebook', 'github', 'linkedin']
+  return (
+    <>
+    <div className='tarjetaForm'>
+        <h3 className='Titulo'>Crear una cuenta</h3> 
+        <SocialButton icons={iconos}/>
+        <Formulario/>  
+        <Alert/>
+    </div>
+    </>
+  )
+}
+
+export default Registro
---
+++ desafioCyE/src/components/SocialButton.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SocialButton = (props) => {
+  return (
+    <div className='RRSS'>
+      {props.icons.map((icon, index) => (
+      <i key={index} className={`fa-brands fa-${icon} fa-2xl` }></i>
+   
+      ))}
+    </div>
+  )
+}
+
+export default SocialButton
