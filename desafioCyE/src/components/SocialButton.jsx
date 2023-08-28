98cf01a23b02086b797337c548e27fd34e009b66
Nicoarratia <nicolas.andres.arratia@gmail.com>
2023-08-28 19:19:32 -0400

    Corrección tamaño de iconos #

--- desafioCyE/src/components/SocialButton.jsx
+++ desafioCyE/src/components/SocialButton.jsx
@@ -4,7 +4,7 @@ const SocialButton = (props) => {
   return (
     <div className='RRSS'>
       {props.icons.map((icon, index) => (
-        <i key={index} className={`fa-brands fa-${icon} fa-2xl`}></i>
+        <i key={index} className={`fa-brands fa-${icon} fa-3x`}></i>
 
       ))}
     </div>
